7d04ea2030b6ff61da70da9352bfd1d9e144f5c2
npsk26 <my_nutthapat-26_@hotmail.com>
2019-05-03 10:29:34 +0700

    check logon 👌🐱‍🏍

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './App.css'
 import TapBar from './components/items/FromChat/TapBar'
-import FaceI from './components/items/FaceI'
+// import FaceI from './components/items/FaceI'
 import { withStyles } from '@material-ui/core/styles'
-import AppI from './components/items/FromChat/AppI'
+// import AppI from './components/items/FromChat/AppI'
+
+import { BrowserRouter as Router } from "react-router-dom";
+// Firebase.
+import * as firebase from 'firebase';
+// import { RoutePages } from './RoutePages'
+// firebase ui
+import FirebaseAuth from './components/SignInAndUp/FirebaseAuth'
+import AppBarBottom from './components/SignInAndUp/AppBarBottom'
+import BackgrourdFromSingInAndUp from './components/SignInAndUp/BackgrourdFromSingInAndUp'
+
+// firebase app connect
+import firebaseApp from './Firbase/firebaseApp'
+
 const stylesI = theme => ({
     BGcolor: {
         backgroundColor: '#6999E2',
@@ -13,8 +26,44 @@ class App extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = { a: 0 }
+        this.state = {
+            isSignedIn: undefined,
+        }
+
+        this.uiConfig = {
+            signInFlow: 'popup',
+            signInOptions: [
+                firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.PhoneAuthProvider.PROVIDER_ID
+            ],
+            callbacks: {
+                signInSuccessWithAuthResult: () => false,
+            },
+        };
     }
+
+
+
+
+
+    /**
+     * @inheritDoc
+     */
+    componentDidMount() {
+
+        firebaseApp.auth().onAuthStateChanged((user) => {
+            this.setState({ isSignedIn: user });
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    // componentWillUnmount() {
+    //     this.unregisterAuthObserver();
+    // }
 
     componentDidMount() {
 
@@ -23,11 +72,35 @@ class App extends React.Component {
 
 
         return (
-            <React.Fragment>
-                {/* <AppI></AppI> */}
-                {/* <FaceI/>   */}
-                <TapBar></TapBar>
-            </React.Fragment>
+
+            <Router>
+                {this.state.isSignedIn !== undefined && !this.state.isSignedIn &&
+                    <div>
+                        <BackgrourdFromSingInAndUp >
+
+                        </BackgrourdFromSingInAndUp>
+                        <AppBarBottom>
+                            <FirebaseAuth uiConfig={this.uiConfig}
+                                firebaseAuth={firebaseApp.auth()} />
+
+                            {/* <Facebook /> */}
+                        </AppBarBottom>
+                    </div>
+                }
+                {this.state.isSignedIn &&
+                    <div >
+                        {/* <AppI></AppI> */}
+                        {/* <FaceI/>   */}
+                        <TapBar></TapBar>
+
+                        Hello {firebaseApp.auth().currentUser.displayName}. You are now signed In!
+        <a onClick={() => firebaseApp.auth().signOut()}>Sign-out</a>
+                        {/* <RoutePages /> */}
+                    </div>
+                }
+
+            </Router>
+
         )
     }
 }
