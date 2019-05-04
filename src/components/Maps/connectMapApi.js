f5ff1260760878da5d81660a12d6a3c97f31803c
npsk26 <my_nutthapat-26_@hotmail.com>
2019-05-04 15:24:40 +0700

    add comment  💬

--- src/components/Maps/connectMapApi.js
+++ src/components/Maps/connectMapApi.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import loadScriptMaps from './loadScriptMaps'
 
+
+// connect map api from YOUR_API_KEY
+// and create maps by initMap
+
 function connectMapApi(YOUR_API_KEY, initMap) {
     loadScriptMaps(`https://maps.googleapis.com/maps/api/js?key=${YOUR_API_KEY}&callback=initMap`)
 
