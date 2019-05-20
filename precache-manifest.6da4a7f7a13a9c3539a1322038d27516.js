10bd6b2f4c72c7270909595bdad1b084d5ee63bc
npsk26 <my_nutthapat-26_@hotmail.com>
2019-05-21 04:09:41 +0700

    change url 🎈

--- precache-manifest.6da4a7f7a13a9c3539a1322038d27516.js
+++ precache-manifest.6da4a7f7a13a9c3539a1322038d27516.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "461321d0e6b53334ae94",
-    "url": "./static/js/runtime~main.81be644b.js"
+    "url": "/share-to-go-ui/static/js/runtime~main.81be644b.js"
   },
   {
     "revision": "d45fbc0b8af5a494e929",
-    "url": "./static/js/main.612c88fb.chunk.js"
+    "url": "/share-to-go-ui/static/js/main.612c88fb.chunk.js"
   },
   {
     "revision": "52dd36c5739db1df4b79",
-    "url": "./static/js/2.20cbf168.chunk.js"
+    "url": "/share-to-go-ui/static/js/2.20cbf168.chunk.js"
   },
   {
     "revision": "d45fbc0b8af5a494e929",
-    "url": "./static/css/main.60842b19.chunk.css"
+    "url": "/share-to-go-ui/static/css/main.60842b19.chunk.css"
   },
   {
     "revision": "8b0afa3b676f15b1b204790d088abda3",
-    "url": "./index.html"
+    "url": "/share-to-go-ui/index.html"
   }
 ];
