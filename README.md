2b70b89a9c5ae7c95d2d4db96adaab4bb63df510
npsk26 <my_nutthapat-26_@hotmail.com>
2019-05-21 07:24:44 +0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# share-to-go-ui
---
+++ precache-manifest.48c8dcd0f25976e7faf2f4979c141481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "461321d0e6b53334ae94",
+    "url": "/share-to-go-ui/static/js/runtime~main.81be644b.js"
+  },
+  {
+    "revision": "dc1f3dacb33038a19568",
+    "url": "/share-to-go-ui/static/js/main.583b8a57.chunk.js"
+  },
+  {
+    "revision": "52dd36c5739db1df4b79",
+    "url": "/share-to-go-ui/static/js/2.20cbf168.chunk.js"
+  },
+  {
+    "revision": "dc1f3dacb33038a19568",
+    "url": "/share-to-go-ui/static/css/main.60842b19.chunk.css"
+  },
+  {
+    "revision": "ba72895b74b9514f048732d52aa96075",
+    "url": "/share-to-go-ui/index.html"
+  }
+];
